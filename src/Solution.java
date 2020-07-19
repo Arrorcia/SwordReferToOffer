0eb9587fe31e7e6b1ad634b12d5960015822df2c
Ame-lm <ame-lm@qq.com>
2020-07-19 11:08:43 +0800

    add solution of JZ-01:二维数组中的查找

--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,20 @@
 public class Solution {
+    //JZ-01. 二维数组中的的查找
+    public boolean Find(int target, int[][] array) {
+        if (array == null || array.length < 1 || array[0].length < 1) {
+            return false;
+        }
+        int i = 0;
+        int j = array[0].length - 1;
+        while (i < array.length && j >= 0) {
+            if (array[i][j] < target) {
+                i++;
+            } else if (array[i][j] > target) {
+                j--;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
 }
